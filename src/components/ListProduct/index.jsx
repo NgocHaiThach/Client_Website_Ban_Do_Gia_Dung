6ca723f58bd322c46ed98c0feac2bdc387ea866b
NgocHai <ngchai2410@gmail.com>
2022-07-15 15:08:37 +0700

    update

--- src/components/ListProduct/index.jsx
+++ src/components/ListProduct/index.jsx
@@ -29,6 +29,29 @@ function ListProduct({ listProduct }) {
                                                 <h4 className="product-propose_new-item_price">{formatPrice(item.price)}đ</h4>
                                             </Link>
                                         </div>
+                                        {/* <div className="product-item__detail">
+                                            <div className="product-wrapper">
+                                                <div className="product-frame">
+                                                    <Link className='product-frame__link' style={{ textDecoration: "none" }} to={`/product/${item.productId}`}>
+                                                        <img src={item.avatar}
+                                                            alt="tokyolife" className="product-frame-item_img" />
+                                                    </Link>
+                                                </div>
+                                                <div className="product-outer">
+                                                    <h3 className="product-reset">
+                                                        <Link className="product-reset__name" style={{ textDecoration: "none" }} to={`/product/${item.productId}`}>
+                                                            {item.name}
+                                                        </Link>
+                                                    </h3>
+                                                    <div className="product-model">
+                                                        Mã: {item.productId}
+                                                    </div>
+                                                    <div className="product-price">
+                                                        {item.price}đ
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div> */}
                                     </div>
                                 ))}
                             </div>
